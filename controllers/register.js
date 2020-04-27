b4ffdd11d7b70d38ed7aacd1723ae9629cfb8a75
sanyavanya <ivanov.unn@gmail.com>
2020-04-27 12:25:02 +0300

    fixed error message for unreached database

--- controllers/register.js
+++ controllers/register.js
@@ -25,7 +25,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 		.catch(trx.rollback)
 	})
 
-	.catch(err => res.status(400).json(err))
+	.catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
