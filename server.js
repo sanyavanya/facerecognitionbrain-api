4f54ccfe3d2505d4e0242452028a1c251acd0287
sanyavanya <ivanov.unn@gmail.com>
2020-04-27 12:02:05 +0300

    fixed res.sent to res.send

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.sent('it is working')})
+app.get('/', (req, res) => { res.send('it is working')})
 
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt))
 
